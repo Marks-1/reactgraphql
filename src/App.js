7051d5fc26fe79863c75d506a26420b3f787e595
onyachmark <onyachmark@gmail.com>
2022-01-11 07:18:18 +0300

    Express server setup

--- src/App.js
+++ src/App.js
@@ -6,47 +6,45 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import RepoInfo from "./RepoInfo";
 
-
 function App() {
-  let [userName, setUserName] = useState('')
-  let [repoList, setRepoList] = useState(null)
+  let [userName, setUserName] = useState("");
+  let [repoList, setRepoList] = useState(null);
 
   let fetchData = useCallback(() => {
     fetch(github.baseURL, {
-      method: 'POST',
+      method: "POST",
       headers: github.headers,
       body: JSON.stringify(query),
     })
-    .then(response => response.json())
-    .then(data =>{
-      let viewer = data.data.viewer
-      const repos = data.data.search.nodes
-      setUserName(viewer.name)
-      setRepoList(repos)
-      console.log(data)
-    })
-    .catch(err =>{
-      console.log(err)
-    })
-    },[],)
+      .then((response) => response.json())
+      .then((data) => {
+        let viewer = data.data.viewer;
+        const repos = data.data.search.nodes;
+        setUserName(viewer.name);
+        setRepoList(repos);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  useEffect(()=>{
-    fetchData()
-  }, [fetchData])
-
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="App container">
-      <Header/>
+      <Header />
       <h4>Hey {userName}</h4>
       {repoList && (
         <ul className="list-group list-group-flush">
-          {repoList.map((repo)=>(
-            <RepoInfo key={repo.id} repo={repo}/>
+          {repoList.map((repo) => (
+            <RepoInfo key={repo.id} repo={repo} />
           ))}
         </ul>
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
